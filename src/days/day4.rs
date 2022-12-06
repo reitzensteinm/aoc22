4e5cd09592dfde2e6997bf27a1b8943f96c94817
reitzensteinm <reitzensteinm@gmail.com>
2022-12-06 21:07:23 +1100

    Day 4

---
+++ src/days/day4.rs
@@ -0,0 +1,50 @@
+use regex::Regex;
+use std::fs::read_to_string;
+
+#[derive(Copy, Clone, Debug)]
+struct Range {
+    from: u32,
+    to: u32,
+}
+
+impl Range {
+    pub fn new(from: u32, to: u32) -> Range {
+        Range { from, to }
+    }
+    pub fn contains(self, other: Range) -> bool {
+        other.from >= self.from && other.to <= self.to
+    }
+
+    pub fn overlaps(self, other: Range) -> bool {
+        let lower = (other.from < self.from && other.to < self.from);
+        let higher = (other.from > self.to && other.to > self.to);
+
+        !(lower || higher)
+    }
+}
+
+pub fn day_4() -> (String, String) {
+    let f = read_to_string("input/day4.txt").unwrap();
+
+    let re = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+
+    let mut fully_contains = 0;
+    let mut overlaps = 0;
+
+    for l in f.split("\n").take_while(|l| l.len() > 0) {
+        let c = re.captures(l).unwrap();
+
+        let a = Range::new(str::parse(&c[1]).unwrap(), str::parse(&c[2]).unwrap());
+        let b = Range::new(str::parse(&c[3]).unwrap(), str::parse(&c[4]).unwrap());
+
+        if a.contains(b) || b.contains(a) {
+            fully_contains += 1;
+        }
+
+        if a.overlaps(b) {
+            overlaps += 1;
+        }
+    }
+
+    (format!("{}", fully_contains), format!("{}", overlaps))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use crate::days::day1::day_1;
 use crate::days::day2::day_2;
 use crate::days::day3::day_3;
+use crate::days::day4::day_4;
 use std::time;
 
 mod days;
 
 fn main() {
-    println!("Hello, world!");
-
     let start = time::Instant::now();
     let run_day = |d: &dyn Fn() -> (String, String)| {
         println!("{:?}", d());
-        println!("{}", (time::Instant::now() - start).as_millis());
+        println!("{}", (time::Instant::now() - start).as_micros());
     };
 
     run_day(&day_1);
     run_day(&day_2);
     run_day(&day_3);
+    run_day(&day_4);
 }
