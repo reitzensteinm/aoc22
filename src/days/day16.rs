ad0bb945464ed33abc606a09cf0088c0c69687e4
reitzensteinm <reitzensteinm@gmail.com>
2022-12-23 18:23:29 +1100

    Progress

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use regex::Regex;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::time;
 
@@ -75,72 +75,65 @@ struct SearchState {
     enabled: u64,
     volume: u32,
     step: u8,
+    reward: usize,
 }
 
 impl PartialOrd for SearchState {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let ratio = |v: &SearchState| v.volume / (v.step.max(1) as u32);
-        // Some(ratio(self).cmp(&ratio(other)))
-        //Some(self.volume.cmp(&other.volume))
-        //Some(other.step.cmp(&self.step))
-        Some(self.step.cmp(&other.step))
-        // Some(
-        //     other
-        //         .step
-        //         .cmp(&self.step)
-        //         .then(other.volume.cmp(&self.volume)),
-        // )
+        Some(
+            self.step
+                .cmp(&other.step)
+                .then(self.reward.cmp(&other.reward)),
+        )
     }
 }
 
 impl SearchState {
-    // fn essential(&self, optimistic: bool) -> (u8, u8, u8, u64) {
-    //     let mut res = (self.step, self.a.pos, self.b.pos, self.enabled);
-    //     if optimistic {
-    //         if let Some((dst, b)) = self.a.dest {
-    //             res.1 = dst;
-    //         }
-    //         if let Some((dst, b)) = self.b.dest {
-    //             res.2 = dst;
-    //         }
-    //     }
-    //     res
-    // }
-
-    fn steel_man(&self) -> SearchState {
-        let mut res = self.clone();
-        if let Some((dst, b)) = res.a.dest {
-            res.a.pos = dst;
-            res.a.dest = None;
-            res.enabled |= (1 << dst);
-        }
-        if let Some((dst, b)) = res.b.dest {
-            res.b.pos = dst;
-            res.b.dest = None;
-            res.enabled |= (1 << dst);
-        }
-        res
-    }
-
-    fn strictly_superior_to(&self, search: &Search, other: &SearchState) -> bool {
-        if other.step < self.step {
-            return false;
+    fn calculate_reward(&mut self, search: &Search) {
+        let mut reward = self.total_flow(search) as usize;
+        if let Some((score, dist)) = self.a.dest {
+            reward += score as usize / dist as usize;
         }
 
-        let vol_adjust = self.volume + self.total_flow(search) * (other.step - self.step) as u32;
-
-        vol_adjust >= other.volume
+        if let Some((score, dist)) = self.b.dest {
+            reward += score as usize / dist as usize;
+        }
+        self.reward = reward;
     }
 
     fn total_flow(&self, search: &Search) -> u32 {
         let mut out = 0;
-        for x in 0..64 {
-            if (self.enabled >> x) & 1 == 1 {
-                let valve = &search.system.valves[x];
+        for x in &search.system.flow_priority {
+            if (self.enabled >> *x) & 1 == 1 {
+                let valve = &search.system.valves[*x];
                 out += valve.flow;
             }
         }
         out as u32
+    }
+
+    fn accelerate(&mut self, search: &Search) {
+        while let (Some((dest_a, prog_a)), Some((dest_b, prog_b))) = (self.a.dest, self.b.dest) {
+            if self.step >= 26 {
+                return;
+            }
+            self.volume += self.total_flow(search);
+            self.step += 1;
+            if prog_a > 1 {
+                self.a.dest = Some((dest_a, prog_a - 1));
+            } else {
+                self.enabled |= 1 << dest_a;
+                self.a.pos = dest_a;
+                self.a.dest = None;
+            }
+            if prog_b > 1 {
+                self.b.dest = Some((dest_b, prog_b - 1));
+            } else {
+                self.enabled |= 1 << dest_b;
+                self.b.pos = dest_b;
+                self.b.dest = None;
+            }
+        }
     }
 
     fn choices(&self, search: &Search) -> Vec<SearchState> {
@@ -148,13 +141,13 @@ impl SearchState {
             Continue,
             Idle,
             Open(u8, u8),
-        };
+        }
 
         if self.step >= 26 {
             return vec![];
         }
 
-        let build_choices = |e: &Entity| {
+        let build_choices = |e: &Entity, other: &Entity| {
             if e.dest.is_some() {
                 return vec![Choice::Continue];
             } else {
@@ -163,12 +156,14 @@ impl SearchState {
                     if (self.enabled >> v) & 1 == 0 {
                         let delay = search.system.valves[e.pos as usize].times[*v] as u8;
 
+                        if let Some((p, _)) = other.dest {
+                            if p as usize == *v {
+                                continue;
+                            }
+                        }
                         if self.step + delay + 2 > 26 {
                         } else {
-                            out.push(Choice::Open(
-                                *v as u8, delay,
-                                //+ 1,
-                            ));
+                            out.push(Choice::Open(*v as u8, delay));
                         }
                     }
                 }
@@ -181,17 +176,22 @@ impl SearchState {
 
         let mut out = vec![];
         let flow = self.total_flow(search);
-        for my_choice in build_choices(&self.a) {
-            for ele_choice in build_choices(&self.b) {
+        for my_choice in build_choices(&self.a, &self.b) {
+            for ele_choice in build_choices(&self.b, &self.a) {
+                if self.a.pos == self.b.pos {
+                    if let Choice::Open(ta, _) = my_choice {
+                        if let Choice::Open(tb, _) = ele_choice {
+                            if tb <= ta {
+                                continue;
+                            }
+                        }
+                    }
+                }
+
                 let mut base = self.clone();
+
                 base.volume += flow;
                 base.step += 1;
-                //
-                // let mut apply = |entity:&Entity,choice:&Choice| {
-                //
-                // };
-
-                //apply(&mut self.a, &mut self.b);
 
                 match my_choice {
                     Choice::Open(dest, len) => {
@@ -204,7 +204,7 @@ impl SearchState {
                         if prog > 1 {
                             base.a.dest = Some((dest, prog - 1));
                         } else {
-                            base.enabled |= (1 << dest);
+                            base.enabled |= 1 << dest;
                             base.a.pos = dest;
                             base.a.dest = None;
                         }
@@ -222,25 +222,14 @@ impl SearchState {
                         if prog > 1 {
                             base.b.dest = Some((dest, prog - 1));
                         } else {
-                            base.enabled |= (1 << dest);
+                            base.enabled |= 1 << dest;
                             base.b.pos = dest;
                             base.b.dest = None;
                         }
                     }
                 }
 
-                //
-                // if let Choice::Move(sq) = my_choice {
-                //     base.a.pos = sq;
-                // } else {
-                //     base.enabled |= (1 << self.a.pos);
-                // }
-                //
-                // if let Choice::Move(sq) = ele_choice {
-                //     base.b.pos = sq;
-                // } else {
-                //     base.enabled |= (1 << self.b.pos);
-                // }
+                base.calculate_reward(search);
 
                 out.push(base);
             }
@@ -256,40 +245,97 @@ struct Search {
 }
 
 impl SearchState {
-    //
-
     fn best_possible_outcome(&self, search: &Search) -> usize {
         let steps = (26 - self.step) as usize;
 
         let mut clone = self.clone();
         let mut out = self.volume as usize;
 
+        let a_first = self.a.dest.map(|i| i.1 - 1).unwrap_or(0) as usize;
+        let b_first = self.b.dest.map(|i| i.1 - 1).unwrap_or(0) as usize;
+
+        let a_specific: Option<u8> = self.a.dest.map(|i| i.0);
+        let b_specific: Option<u8> = self.b.dest.map(|i| i.0);
+
+        let a_next = if let Some(ap) = a_specific {
+            let mut lowest = None;
+            for p in &search.system.flow_priority {
+                if (clone.enabled >> p) & 1 == 0 && (*p != ap as usize) {
+                    let time = search.system.valves[ap as usize].times[*p];
+                    if let Some(v) = lowest {
+                        if time < v {
+                            lowest = Some(time);
+                        }
+                    } else {
+                        lowest = Some(time);
+                    }
+                }
+            }
+            a_first + lowest.unwrap_or(1) + 1
+        } else {
+            a_first + 3
+        };
+
+        let b_next = if let Some(ap) = b_specific {
+            let mut lowest = None;
+            for p in &search.system.flow_priority {
+                if (clone.enabled >> p) & 1 == 0 && (*p != ap as usize) {
+                    let time = search.system.valves[ap as usize].times[*p];
+                    if let Some(v) = lowest {
+                        if time < v {
+                            lowest = Some(time);
+                        }
+                    } else {
+                        lowest = Some(time);
+                    }
+                }
+            }
+            b_first + lowest.unwrap_or(1) + 1
+        } else {
+            b_first + 3
+        };
+
+        let generic_assign = |clone: &mut SearchState| {
+            for p in &search.system.flow_priority {
+                if (clone.enabled >> p) & 1 == 0 {
+                    clone.enabled |= 1 << p;
+                    break;
+                }
+            }
+        };
+
         for c in 0..steps {
             out += clone.total_flow(search) as usize;
 
-            if c % 3 == 0 {
-                let mut applied = 0;
-                for p in &search.system.flow_priority {
-                    if (clone.enabled >> p) & 1 == 0 {
-                        applied += 1;
-                        if applied > 2 {
-                            break;
-                        }
-                        clone.enabled |= (1 << p);
+            let a_trigger = c == a_first || ((c >= a_next) && ((c - a_next) % 3 == 0));
+            let b_trigger = c == b_first || ((c >= b_next) && ((c - b_next) % 3 == 0));
+
+            if a_trigger {
+                if let Some(v) = a_specific {
+                    if c == a_first {
+                        clone.enabled |= 1 << v;
+                    } else {
+                        generic_assign(&mut clone);
                     }
+                } else {
+                    generic_assign(&mut clone);
+                }
+            }
+
+            if b_trigger {
+                if let Some(v) = b_specific {
+                    if c == b_first {
+                        clone.enabled |= 1 << v;
+                    } else {
+                        generic_assign(&mut clone);
+                    }
+                } else {
+                    generic_assign(&mut clone);
                 }
             }
         }
 
-        //println!("{:?}", (time::Instant::now() - start).as_micros());
         out
-
-        // let steps = (26 - self.step) as usize;
-        // let mut out = 0;
-        // for v in &search.system.valves {
-        //     out += v.flow * steps;
-        // }
-        // out + self.volume as usize
     }
 }
 
@@ -298,55 +344,12 @@ impl Search {
         let mut heap = BinaryHeap::new();
         heap.push(self.initial);
 
-        let mut highest_scores = HashMap::<(u8, u8, u8, u64), usize>::new();
-
-        let mut best_states = HashMap::<u64, SearchState>::new();
-
         let mut best: Option<SearchState> = None;
         let mut c = 0_usize;
         let mut skipped = 0_usize;
-        let mut skipped_best = 0_usize;
 
-        while let Some(ss) = heap.pop() {
-            //println!("{} {}", ss.step, ss.volume);
-
+        while let Some(mut ss) = heap.pop() {
             c += 1;
-            if c % 1_000_000 == 0 {
-                println!("{} {} {} {}", heap.len(), skipped, skipped_best, c);
-                println!(
-                    "Best {} {} {}",
-                    ss.step,
-                    ss.best_possible_outcome(self),
-                    best.map(|v| v.volume).unwrap_or(0)
-                );
-                println!("BSL: {}", best_states.len());
-            }
-
-            // if let Some(s) = highest_scores.get(&ss.essential(true)) {
-            //     if *s >= ss.volume as usize {
-            //         skipped_best += 1;
-            //         continue;
-            //     }
-            // }
-            //
-            // highest_scores.insert(ss.essential(false), ss.volume as usize);
-
-            // let steel = ss.steel_man();
-            // if let Some(s) = best_states.get(&steel.enabled) {
-            //     if s.strictly_superior_to(&self, &ss) {
-            //         skipped_best += 1;
-            //         continue;
-            //     }
-            // }
-            //
-            // if let Some(s) = best_states.get(&ss.enabled) {
-            //     if ss.strictly_superior_to(&self, s) {
-            //         best_states.insert(ss.enabled, ss.clone());
-            //     }
-            // } else {
-            //     best_states.insert(ss.enabled, ss.clone());
-            // }
-
             if let Some(b) = best {
                 let best_outcome = ss.best_possible_outcome(self);
                 if b.volume >= best_outcome as u32 {
@@ -355,10 +358,11 @@ impl Search {
                 }
             }
 
+            ss.accelerate(&self);
+
             if ss.step == 26 {
                 if let Some(s) = best {
                     if s.volume < ss.volume {
-                        //println!("new best {}", ss.volume);
                         best = Some(ss);
                     }
                 } else {
@@ -369,47 +373,14 @@ impl Search {
                     heap.push(c);
                 }
             }
-            //println!("{}", ss.volume);
-            //println!("{:?}", heap);
         }
 
-        println!("{:?}", best);
-        println!("{:#018b}", best.unwrap().enabled);
+        println!("{}", c);
+        // println!("{:?}", best);
+        // println!("{:#018b}", best.unwrap().enabled);
 
         best.unwrap().volume
-        //todo!();
     }
-
-    // fn search(&mut self) -> u32 {
-    //     let mut next = vec![self.initial];
-    //
-    //     loop {
-    //         let mut step = HashMap::<(u8, u8, u64), SearchState>::new();
-    //         for n in &next {
-    //             for c in n.choices(self) {
-    //                 if let Some(oc) = step.get(&c.essential()) {
-    //                     if c.volume > oc.volume {
-    //                         step.insert(c.essential(), c);
-    //                     }
-    //                 } else {
-    //                     step.insert(c.essential(), c);
-    //                 }
-    //             }
-    //         }
-    //
-    //         println!("{}", step.len());
-    //
-    //         if step.len() == 0 {
-    //             break;
-    //         }
-    //
-    //         next = Vec::from_iter(step.values().map(|v| *v));
-    //     }
-    //
-    //     next.sort_by(|va, vb| va.volume.cmp(&vb.volume));
-    //
-    //     next.last().unwrap().volume
-    // }
 }
 
 pub fn day_16() -> (String, String) {
@@ -426,7 +397,6 @@ pub fn day_16() -> (String, String) {
             *i
         } else {
             let i = mapping.len();
-            println!("{} {}", i, m);
             mapping.insert(m.to_string(), i);
             i
         }
@@ -470,13 +440,12 @@ pub fn day_16() -> (String, String) {
                 pos: get_mapping("AA") as u8,
                 dest: None,
             },
+            reward: 0,
             enabled: 0,
             volume: 0,
             step: 0,
         },
     };
 
-    (format!("{}", search.search()), "".to_string());
-    println!("{:?}", (time::Instant::now() - start).as_micros());
-    panic!();
+    (format!("{}", search.search()), "".to_string())
 }
