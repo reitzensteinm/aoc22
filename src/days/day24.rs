5bc51228c5a9d04034495a49a3f7feb06f61ae06
reitzensteinm <reitzensteinm@gmail.com>
2023-04-16 22:55:35 +1000

    Day 24 Bitgrid

--- src/days/day24.rs
+++ src/days/day24.rs
@@ -1,157 +1,5 @@
+use crate::utils::bit_grid::{BitGrid, BitView, Or, Prim, Shifted, ShiftedWrap, Window};
 use std::fs::read_to_string;
-
-// Dynamic Programming today, to change things up. My intuition is a tree search like A* may well
-// end up faster - you're mostly going to be hammering down and to the right so should get an
-// excellent outcome depth first. But I like this solution too.
-// This one however would be pretty trivial to vectorize. You could fit the play field
-// IN REGISTERS on an AVX-512 machine.
-
-#[derive(Copy, Clone, PartialEq)]
-enum Square {
-    Empty,
-    Wall,
-    BlizzardNorth,
-    BlizzardSouth,
-    BlizzardEast,
-    BlizzardWest,
-}
-
-struct Field {
-    width: usize,
-    height: usize,
-    squares: Vec<Square>,
-}
-
-fn wrap(n: isize, div: isize) -> isize {
-    let o = n % div;
-    if o < 0 {
-        div + o
-    } else {
-        o
-    }
-}
-
-impl Field {
-    fn wrap_blizzard(&self, (px, py): (usize, usize), (dx, dy): (isize, isize)) -> (usize, usize) {
-        (
-            (wrap((px as isize + dx) - 1, (self.width - 2) as isize) + 1) as usize,
-            (wrap((py as isize + dy) - 1, (self.height - 2) as isize) + 1) as usize,
-        )
-    }
-
-    fn get_square(&self, (px, py): (usize, usize)) -> Square {
-        self.squares[py * self.width + px]
-    }
-
-    fn blocked(&self, pos: (usize, usize), ticks: usize) -> bool {
-        if self.get_square(pos) == Square::Wall {
-            return true;
-        }
-
-        if pos.0 >= 1 && pos.0 < self.width - 1 && pos.1 >= 1 && pos.1 < self.height - 1 {
-            if self.get_square(self.wrap_blizzard(pos, (-(ticks as isize), 0)))
-                == Square::BlizzardEast
-            {
-                return true;
-            }
-
-            if self.get_square(self.wrap_blizzard(pos, ((ticks as isize), 0)))
-                == Square::BlizzardWest
-            {
-                return true;
-            }
-
-            if self.get_square(self.wrap_blizzard(pos, (0, -(ticks as isize))))
-                == Square::BlizzardSouth
-            {
-                return true;
-            }
-            if self.get_square(self.wrap_blizzard(pos, (0, (ticks as isize))))
-                == Square::BlizzardNorth
-            {
-                return true;
-            }
-        }
-
-        false
-    }
-}
-
-struct Pathfind {
-    field: Field,
-}
-
-#[derive(Clone)]
-struct PathfindState {
-    ticks: usize,
-    width: usize,
-    height: usize,
-    reachable: Vec<bool>,
-}
-
-impl PathfindState {
-    fn iterate(&self, field: &Field) -> PathfindState {
-        let mut pfs = self.clone();
-        pfs.ticks += 1;
-
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let blocked = field.blocked((x, y), pfs.ticks);
-                let mut neighbour = false;
-
-                for dy in -1..=1 {
-                    for dx in -1..=1 {
-                        if dx != 0 && dy != 0 {
-                            continue;
-                        }
-
-                        let nx = x as isize + dx;
-                        let ny = y as isize + dy;
-                        if nx < 0
-                            || ny < 0
-                            || nx >= self.width as isize
-                            || ny >= self.height as isize
-                        {
-                            continue;
-                        }
-
-                        if self.reachable[(ny * self.width as isize + nx) as usize] {
-                            neighbour = true;
-                        }
-                    }
-                }
-
-                let reachable = if blocked { false } else { neighbour };
-
-                pfs.reachable[y * self.width + x] = reachable;
-            }
-        }
-
-        pfs
-    }
-}
-
-impl Pathfind {
-    fn search(&self, start: (usize, usize), end: (usize, usize), start_ticks: usize) -> usize {
-        let mut state = PathfindState {
-            ticks: start_ticks,
-            width: self.field.width,
-            height: self.field.height,
-            reachable: vec![false; self.field.squares.len()],
-        };
-
-        state.reachable[start.1 * state.width + start.0] = true;
-
-        for n in 1.. {
-            state = state.iterate(&self.field);
-            if state.reachable[end.1 * state.width + end.0] {
-                return n;
-            }
-        }
-
-        panic!()
-    }
-}
 
 pub fn day_24() -> (String, String) {
     let f = read_to_string("input/day24.txt").unwrap();
@@ -161,46 +9,81 @@ pub fn day_24() -> (String, String) {
     let width = file_lines[0].len();
     let height = file_lines.len();
 
-    let mut squares = vec![];
-
     let mut start = None;
     let mut end = None;
 
+    let mut wall = BitGrid::new(width, height);
+
+    // N S E W
+    let mut blizzards = vec![BitGrid::new(width, height); 4];
+
     for (y, l) in file_lines.iter().enumerate() {
         for (x, c) in l.chars().enumerate() {
-            squares.push(match c {
-                '#' => Square::Wall,
+            match c {
+                '#' => wall.set(x, y, true),
                 '.' => {
                     if y == 0 {
                         start = Some((x, y));
                     } else if y == file_lines.len() - 1 {
                         end = Some((x, y));
                     }
-                    Square::Empty
                 }
-                '>' => Square::BlizzardEast,
-                '<' => Square::BlizzardWest,
-                '^' => Square::BlizzardNorth,
-                'v' => Square::BlizzardSouth,
-                _ => panic!(),
-            });
+
+                '^' => blizzards[0].set(x, y, true),
+                'v' => blizzards[1].set(x, y, true),
+                '>' => blizzards[2].set(x, y, true),
+                '<' => blizzards[3].set(x, y, true),
+                _ => {}
+            }
         }
     }
 
-    let start = start.unwrap();
-    let end = end.unwrap();
+    let width = wall.width() as isize;
+    let height = wall.height() as isize;
 
-    let field = Field {
-        width,
-        height,
-        squares,
+    let pathfind = |from: (usize, usize), to: (usize, usize), time_off: isize| -> isize {
+        let mut time = time_off;
+        let mut locations = BitGrid::new(width as usize, height as usize);
+        locations.set(from.0, from.1, true);
+        loop {
+            let sa = Shifted::new(&locations, 1, 0);
+            let sb = Shifted::new(&locations, -1, 0);
+            let sc = Shifted::new(&locations, 0, -1);
+            let sd = Shifted::new(&locations, 0, 1);
+
+            let acc = Or::new(&locations, &sa);
+            let acc = Or::new(&acc, &sb);
+            let acc = Or::new(&acc, &sc);
+            let acc = Or::new(&acc, &sd);
+
+            let mut moved_blizzards = vec![];
+            for x in 0..4 {
+                let dir = vec![(0, -time), (0, time), (time, 0), (-time, 0)][x];
+                let w = Window::new(&blizzards[x], 1, 1, width - 2, height - 2);
+                let s = ShiftedWrap::new(&w, dir.0, dir.1);
+                let nw = Window::new(&s, 0, 0, width, height);
+                let out = Shifted::new(&nw, 1, 1);
+                moved_blizzards.push(BitGrid::from_view(&out));
+            }
+
+            let acc = Prim::new(&acc, &moved_blizzards[0], |a, b| a & (!b));
+            let acc = Prim::new(&acc, &moved_blizzards[1], |a, b| a & (!b));
+            let acc = Prim::new(&acc, &moved_blizzards[2], |a, b| a & (!b));
+            let acc = Prim::new(&acc, &moved_blizzards[3], |a, b| a & (!b));
+            let acc = Prim::new(&acc, &wall, |a, b| a & (!b));
+
+            locations = BitGrid::from_view(&acc);
+
+            if locations.get(to.0, to.1) {
+                return time;
+            }
+            time += 1;
+        }
     };
 
-    let pathfind = Pathfind { field };
+    let a = pathfind(start.unwrap(), end.unwrap(), 0);
+    let b = pathfind(end.unwrap(), start.unwrap(), a);
+    let c = pathfind(start.unwrap(), end.unwrap(), b);
 
-    let first = pathfind.search(start.clone(), end.clone(), 0);
-    let second = pathfind.search(end.clone(), start.clone(), first);
-    let third = pathfind.search(start.clone(), end.clone(), second + first);
-
-    (format!("{}", first), format!("{}", first + second + third))
+    (format!("{}", a), format!("{}", c))
 }
--- src/utils/bit_grid.rs
+++ src/utils/bit_grid.rs
@@ -1,17 +1,32 @@
+// This file implements an arbitrarily sized 2D bit grid with operations, such as shift, and,
+// or, window, wrapping shift etc.
+
+// It uses references to ensure zero copying, which is a nice ideal. However actually using the
+// library is a massive pain. Also, you can in many cases end up going accidentally exponential,
+// so it's probably not an actual win. If I did it again, I'd just make it copy, even if high
+// performance was the goal.
+
+// Also there are known bugs. But see above. Don't do this.
+
+type BitWidth = u128;
+const WIDTH: isize = 128;
+const WIDTH_U: usize = 128;
+
+#[derive(Clone)]
 pub struct BitGrid {
-    backing: Vec<Vec<u64>>,
+    backing: Vec<Vec<BitWidth>>,
     width: usize,
     height: usize,
 }
 
 pub trait BitView {
-    fn get_backing(&self, x: isize, y: isize) -> u64;
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth;
     fn width(&self) -> usize;
     fn height(&self) -> usize;
 }
 
 impl BitView for BitGrid {
-    fn get_backing(&self, x: isize, y: isize) -> u64 {
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth {
         let get_backing = |xi: isize| {
             *self
                 .backing
@@ -19,25 +34,17 @@ impl BitView for BitGrid {
                 .map(|v| v.get(xi as usize).unwrap_or(&0))
                 .unwrap_or(&0)
         };
-        //
-        // println!("Getting backing {} {}", x, y);
-        //
-        // println!("slots {} {}", (x + 63) / 64 - 1, (x + 63) / 64);
 
-        let rem = x.rem_euclid(64);
+        let rem = x.rem_euclid(WIDTH);
 
-        // println!("rem {}", x.rem_euclid(64));
-
-        let prev = get_backing((x + 64) / 64 - 1);
-        let this = get_backing((x + 64) / 64);
+        let prev = get_backing((x + WIDTH) / WIDTH - 1);
+        let this = get_backing((x + WIDTH) / WIDTH);
 
         let prev_bits = prev.overflowing_shr(rem as u32).0;
-        let this_bits = match this.overflowing_shl((64 - rem) as u32) {
+        let this_bits = match this.overflowing_shl((WIDTH - rem) as u32) {
             (_, true) => 0,
             (v, _) => v,
         };
-
-        // println!("Bit patterns {:b} {:b}", prev_bits, this_bits);
 
         prev_bits | this_bits
     }
@@ -51,7 +58,58 @@ impl BitView for BitGrid {
     }
 }
 
-struct Shifted<'grid, T>
+pub struct Window<'grid, T>
+where
+    T: BitView,
+{
+    view: &'grid T,
+    x: isize,
+    y: isize,
+    width: isize,
+    height: isize,
+}
+
+impl<'grid, T> Window<'grid, T>
+where
+    T: BitView,
+{
+    pub fn new(view: &'grid T, x: isize, y: isize, width: isize, height: isize) -> Self {
+        Window {
+            view,
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+}
+
+impl<T> BitView for Window<'_, T>
+where
+    T: BitView,
+{
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth {
+        if y < 0 || y >= self.height() as isize {
+            return 0;
+        }
+        let bit_count = self.width - x;
+        let mask = match (1 as BitWidth).overflowing_shl(bit_count as u32) {
+            (v, false) => v - 1,
+            _ => !0,
+        };
+        self.view.get_backing(x + self.x, y + self.y) & mask
+    }
+
+    fn width(&self) -> usize {
+        self.width as usize
+    }
+
+    fn height(&self) -> usize {
+        self.height as usize
+    }
+}
+
+pub struct Shifted<'grid, T>
 where
     T: BitView,
 {
@@ -64,7 +122,7 @@ impl<'grid, T> Shifted<'grid, T>
 where
     T: BitView,
 {
-    fn new(view: &'grid T, x: isize, y: isize) -> Self {
+    pub fn new(view: &'grid T, x: isize, y: isize) -> Self {
         Shifted { view, x, y }
     }
 }
@@ -73,8 +131,16 @@ impl<T> BitView for Shifted<'_, T>
 where
     T: BitView,
 {
-    fn get_backing(&self, x: isize, y: isize) -> u64 {
-        self.view.get_backing(x - self.x, y - self.y)
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth {
+        if y < 0 || y >= self.height() as isize {
+            return 0;
+        }
+        let bit_count = self.view.width() as isize - x;
+        let mask = match (1 as BitWidth).overflowing_shl(bit_count as u32) {
+            (v, false) => v - 1,
+            _ => !0,
+        };
+        self.view.get_backing(x - self.x, y - self.y) & mask
     }
 
     fn width(&self) -> usize {
@@ -86,7 +152,88 @@ where
     }
 }
 
-struct Or<'grid, Ta, Tb>
+pub struct ShiftedWrap<'grid, T>
+where
+    T: BitView,
+{
+    view: &'grid T,
+    x: isize,
+    y: isize,
+}
+
+impl<'grid, T> ShiftedWrap<'grid, T>
+where
+    T: BitView,
+{
+    pub fn new(view: &'grid T, x: isize, y: isize) -> Self {
+        ShiftedWrap { view, x, y }
+    }
+}
+
+impl<T> BitView for ShiftedWrap<'_, T>
+where
+    T: BitView,
+{
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth {
+        if y < 0 || y >= self.height() as isize {
+            return 0;
+        }
+        let width = self.view.width() as isize;
+        let height = self.view.height() as isize;
+        let a = Shifted::new(self.view, self.x.rem_euclid(width), 0);
+        let b = Shifted::new(self.view, self.x.rem_euclid(width) - width, 0);
+
+        Prim::new(&a, &b, |a, b| a | b).get_backing(x, (y - self.y).rem_euclid(height))
+    }
+
+    fn width(&self) -> usize {
+        self.view.width()
+    }
+
+    fn height(&self) -> usize {
+        self.view.height()
+    }
+}
+
+pub struct Prim<'grid, Ta, Tb>
+where
+    Ta: BitView,
+    Tb: BitView,
+{
+    a: &'grid Ta,
+    b: &'grid Tb,
+    prim: fn(BitWidth, BitWidth) -> BitWidth,
+}
+
+impl<'grid, Ta, Tb> Prim<'grid, Ta, Tb>
+where
+    Ta: BitView,
+    Tb: BitView,
+{
+    pub fn new(a: &'grid Ta, b: &'grid Tb, prim: fn(BitWidth, BitWidth) -> BitWidth) -> Self {
+        Prim { a, b, prim }
+    }
+}
+
+impl<Ta, Tb> BitView for Prim<'_, Ta, Tb>
+where
+    Ta: BitView,
+    Tb: BitView,
+{
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth {
+        (self.prim)(self.a.get_backing(x, y), self.b.get_backing(x, y))
+    }
+
+    fn width(&self) -> usize {
+        self.a.width().max(self.b.width())
+    }
+
+    fn height(&self) -> usize {
+        self.a.height().max(self.b.height())
+    }
+}
+
+pub struct Or<'grid, Ta, Tb>
 where
     Ta: BitView,
     Tb: BitView,
@@ -100,7 +247,7 @@ where
     Ta: BitView,
     Tb: BitView,
 {
-    fn new(a: &'grid Ta, b: &'grid Tb) -> Self {
+    pub fn new(a: &'grid Ta, b: &'grid Tb) -> Self {
         Or { a, b }
     }
 }
@@ -110,7 +257,7 @@ where
     Ta: BitView,
     Tb: BitView,
 {
-    fn get_backing(&self, x: isize, y: isize) -> u64 {
+    fn get_backing(&self, x: isize, y: isize) -> BitWidth {
         self.a.get_backing(x, y) | self.b.get_backing(x, y)
     }
 
@@ -126,21 +273,21 @@ where
 impl BitGrid {
     pub fn new(width: usize, height: usize) -> BitGrid {
         BitGrid {
-            backing: vec![vec![0; (width + 63) / 64]; height],
+            backing: vec![vec![0; (width + WIDTH_U - 1) / WIDTH_U]; height],
             width,
             height,
         }
     }
 
     pub fn get(&self, x: usize, y: usize) -> bool {
-        (self.backing[y][x / 64] >> x % 64 & 1) == 1
+        (self.backing[y][x / WIDTH_U] >> x % WIDTH_U & 1) == 1
     }
 
     pub fn set(&mut self, x: usize, y: usize, v: bool) {
         if v {
-            self.backing[y][x / 64] |= 1 << (x % 64)
+            self.backing[y][x / WIDTH_U] |= 1 << (x % WIDTH_U)
         } else {
-            self.backing[y][x / 64] &= !(1 << (x % 64))
+            self.backing[y][x / WIDTH_U] &= !(1 << (x % WIDTH_U))
         }
     }
 
@@ -149,8 +296,8 @@ impl BitGrid {
 
         for y in 0..view.height() {
             let mut line = vec![];
-            for x in 0..(view.width() + 63) / 64 {
-                line.push(view.get_backing(x as isize * 64, y as isize));
+            for x in 0..(view.width() + (WIDTH_U - 1)) / WIDTH_U {
+                line.push(view.get_backing(x as isize * WIDTH, y as isize));
             }
             backing.push(line);
         }
@@ -162,6 +309,7 @@ impl BitGrid {
         }
     }
 
+    #[allow(unused)]
     pub fn print(&self) {
         for y in 0..self.height {
             for x in 0..self.width {
@@ -180,50 +328,75 @@ impl BitGrid {
 mod test {
     use super::*;
     use std::time::Duration;
+
     #[test]
-    fn bit_grid() {
-        let mut b = BitGrid::new(64, 64);
-        b.set(5, 5, true);
-        //b.print();
+    fn window_clamp() {
+        let bit_test = |x_set, backing_x, window_x, window_size| {
+            let mut b = BitGrid::new(128, 1);
+            b.set(x_set, 0, true);
+            let w = Window::new(&b, window_x, 0, window_size, 1);
+            let v = BitGrid::from_view(&w);
+            v.print();
+            v.get_backing(backing_x, 0)
+        };
+
+        assert_eq!(bit_test(3, 0, 0, 2), 0);
+        assert_eq!(bit_test(3, 0, 0, WIDTH), 1 << 3);
+        assert_eq!(bit_test(WIDTH_U + 3, WIDTH, 0, WIDTH + 1), 0);
+        assert_eq!(bit_test(WIDTH_U + 3, 0, 4, WIDTH), 1 << 63);
     }
 
-    #[test]
-    fn bit_grid_init() {
-        let mut b = BitGrid::new(128, 1);
-        b.set(127, 0, true);
+    // Visual Test
+    fn repeat() {
+        let mut b = BitGrid::new(16, 4);
+        b.set(1, 1, true);
 
-        for n in 0..128 {
-            let shiftx1 = Shifted::new(&b, -n, 0);
-            BitGrid::from_view(&shiftx1).print();
+        let mut w = Window::new(&b, 1, 1, 14, 2);
+
+        for n in 0..32 {
+            let s = ShiftedWrap::new(&w, n, n);
+            let nb = BitGrid::from_view(&s);
+
+            let ow = Window::new(&nb, -1, -1, 16, 4);
+
+            let db = BitGrid::from_view(&ow);
+
+            db.print();
+            println!();
+        }
+    }
+
+    // Visual Test
+    fn repeat_2() {
+        {
+            let mut b = BitGrid::new(16, 1);
+            b.set(1, 0, true);
+            b.set(2, 0, true);
+
+            for n in 0..32 {
+                let w = Window::new(&b, 1, 0, 15, 1);
+                let s = ShiftedWrap::new(&w, n, 0);
+                let v = BitGrid::from_view(&s);
+                v.print();
+            }
         }
 
-        return;
+        {
+            let mut b = BitGrid::new(16, 4);
+            b.set(0, 1, true);
+            b.set(0, 2, true);
 
-        // assert_eq!(b.get_backing(0, 1), 0b000000);
-
-        // let shifty = Shifted::new(&b, 0, 1);
-        let shiftx1 = Shifted::new(&b, -1, 0);
-        let shiftx2 = Shifted::new(&b, 1, 0);
-        //
-        // BitGrid::from_view(&shifty).print();
-        //
-        // let fv = BitGrid::from_view(&shiftx);
-        // let or = Or::new(&fv, &b);
-        // BitGrid::from_view(&or).print();
-        //
-        // assert_eq!(shifty.get_backing(0, 1), 0b100000);
-
-        // shiftx1.get_backing(0, 0);
-        // shiftx2.get_backing(0, 0);
-        //
-        // assert_eq!(shiftx1.get_backing(0, 0), 0b10000);
-        // assert_eq!(shiftx2.get_backing(0, 0), 0b1000000);
-
-        BitGrid::from_view(&shiftx1).print();
-        BitGrid::from_view(&shiftx2).print();
+            for n in 0..32 {
+                let w = Window::new(&b, 0, 0, 16, 3);
+                let s = ShiftedWrap::new(&w, 0, n);
+                let v = BitGrid::from_view(&s);
+                println!();
+                v.print();
+            }
+        }
     }
 
-    #[test]
+    // Visual Test
     fn bit_grid_shift() {
         let mut b = BitGrid::new(128, 16);
         b.set(63, 5, true);
@@ -231,10 +404,6 @@ mod test {
 
         for x in 0..1000 {
             std::thread::sleep(Duration::from_millis(100));
-
-            // for y in 0..100 {
-            //     println!();
-            // }
 
             let sa = Shifted::new(&b, 1, 0);
             let sb = Shifted::new(&b, -1, 0);
@@ -246,14 +415,7 @@ mod test {
             let acc = Or::new(&acc, &sc);
             let acc = Or::new(&acc, &sd);
 
-            //            let or = Or::new(&b, &shift);
-
             b = BitGrid::from_view(&acc);
-
-            //
-            // b = BitGrid::from_view(
-            //     Or::new(&b, &shift)
-            // );
 
             b.print();
         }
